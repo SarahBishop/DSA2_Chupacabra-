bfa94088f645fbd0e5edfa9b47d80f22cbe42870
Ben C <bxc3201@rit.edu>
2015-11-03 14:18:26 -0500

    Fixed SetModelMatrix so that it works

--- A10/ReEngApplication-master/E13s_BoundingBoxClass/MyBoundingObjectClass.cpp
+++ A10/ReEngApplication-master/E13s_BoundingBoxClass/MyBoundingObjectClass.cpp
@@ -171,7 +171,55 @@ void MyBoundingObjectClass::ToggleVisible() {
 
 void MyBoundingObjectClass::SetModelMatrix(matrix4 a_m4ToWorld)
 {
+	//If there are no changes in the Model Matrix there is no need
+	//of doing further calculations
+	if (m_m4ToWorld == a_m4ToWorld)
+		return;
+
 	m_m4ToWorld = a_m4ToWorld;
+	//Calculate the vertex that makes the box
+	vector3 v3Corner[8];
+	v3Corner[0] = vector3(m_v3Min.x, m_v3Min.y, m_v3Min.z);
+	v3Corner[1] = vector3(m_v3Max.x, m_v3Min.y, m_v3Min.z);
+	v3Corner[2] = vector3(m_v3Min.x, m_v3Max.y, m_v3Min.z);
+	v3Corner[3] = vector3(m_v3Max.x, m_v3Max.y, m_v3Min.z);
+
+	v3Corner[4] = vector3(m_v3Min.x, m_v3Min.y, m_v3Max.z);
+	v3Corner[5] = vector3(m_v3Max.x, m_v3Min.y, m_v3Max.z);
+	v3Corner[6] = vector3(m_v3Min.x, m_v3Max.y, m_v3Max.z);
+	v3Corner[7] = vector3(m_v3Max.x, m_v3Max.y, m_v3Max.z);
+	//Get vectors in global space
+	for (uint nVertex = 0; nVertex < 8; nVertex++)
+	{
+		v3Corner[nVertex] = vector3(m_m4ToWorld * vector4(v3Corner[nVertex], 1.0f));
+	}
+
+	//Get the max and min out of the list
+	m_v3MaxG = m_v3MinG = v3Corner[0];
+	for (uint nVertex = 1; nVertex < 8; nVertex++)
+	{
+		if (m_v3MinG.x > v3Corner[nVertex].x) //If min is larger than current
+			m_v3MinG.x = v3Corner[nVertex].x;
+		else if (m_v3MaxG.x < v3Corner[nVertex].x)//if max is smaller than current
+			m_v3MaxG.x = v3Corner[nVertex].x;
+
+		if (m_v3MinG.y > v3Corner[nVertex].y) //If min is larger than current
+			m_v3MinG.y = v3Corner[nVertex].y;
+		else if (m_v3MaxG.y < v3Corner[nVertex].y)//if max is smaller than current
+			m_v3MaxG.y = v3Corner[nVertex].y;
+
+		if (m_v3MinG.z > v3Corner[nVertex].z) //If min is larger than current
+			m_v3MinG.z = v3Corner[nVertex].z;
+		else if (m_v3MaxG.z < v3Corner[nVertex].z)//if max is smaller than current
+			m_v3MaxG.z = v3Corner[nVertex].z;
+	}
+	m_v3CenterG = (m_v3MinG + m_v3MaxG) / 2.0f;
+
+	//we calculate the distance between all the values of min and max vectors
+	m_v3HalfWidthG.x = glm::distance(vector3(m_v3MinG.x, 0.0f, 0.0f), vector3(m_v3MaxG.x, 0.0f, 0.0f)) / 2.0f;
+	m_v3HalfWidthG.y = glm::distance(vector3(0.0f, m_v3MinG.y, 0.0f), vector3(0.0f, m_v3MaxG.y, 0.0f)) / 2.0f;
+	m_v3HalfWidthG.z = glm::distance(vector3(0.0f, 0.0f, m_v3MinG.z), vector3(0.0f, 0.0f, m_v3MaxG.z)) / 2.0f;
+
 }
 matrix4 MyBoundingObjectClass::GetModelMatrix(void)
 {
