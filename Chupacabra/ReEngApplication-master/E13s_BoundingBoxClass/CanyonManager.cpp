63e73763712f46f20acbd11fdcc6e92a341d2483
unknown <ebs9787@GDD-51.main.ad.rit.edu>
2015-11-15 15:44:20 -0500

    Eric - Made canyon colors more sand-like.

--- Chupacabra/ReEngApplication-master/E13s_BoundingBoxClass/CanyonManager.cpp
+++ Chupacabra/ReEngApplication-master/E13s_BoundingBoxClass/CanyonManager.cpp
@@ -11,8 +11,8 @@ void CanyonManager::GenerateCanyon(uint nSegments)
 	srand(time(NULL));
 	for (int i = 0; i < nSegments; i++)
 	{
-		float brightness = (rand() % 399 + 400) / 1000.f;
-		listOfSegments.push_back(CanyonSegment(vector3(0.0f, 0.0f, i * segmentZLength), vector3(brightness , brightness * 0.5f, 0.1f), segmentZLength));
+		float brightness = (rand() % 399 + 600) / 1000.f; // originally (rand() % 399 + 400) / 1000.f -- you can change the colors back if you want
+		listOfSegments.push_back(CanyonSegment(vector3(0.0f, 0.0f, i * segmentZLength), vector3(brightness, brightness * 0.68f, brightness * 0.3f), segmentZLength)); //originally vector3(brightness, brightness * 0.5f, 0.1f) for RIT colors
 	}
 
 	farPlane = -segmentZLength * (nSegments - 1) + cameraDepth; // define farPlane 
@@ -36,8 +36,8 @@ void CanyonManager::Update()
 			listOfSegments.at(i).v3Position = vector3(0.0f, 0.0f, nearPlane);
 			
 			//change canyon color
-			float brightness = (rand() % 399 + 400) / 1000.f;
-			listOfSegments.at(i).v3Color = vector3(brightness, brightness * 0.5f, 0.1f);
+			float brightness = (rand() % 399 + 600) / 1000.f; // originally (rand() % 399 + 400) / 1000.f
+			listOfSegments.at(i).v3Color = vector3(brightness, brightness * 0.68f, brightness * 0.3f); //originally vector3(brightness, brightness * 0.5f, 0.1f) for RIT colors
 		}
 	}
 }
